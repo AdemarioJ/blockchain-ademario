ec35a534bb339cbfe9e76b75bdb4e9474d4661fe
ademario <ademariocc@gmail.com>
2019-04-19 03:18:17 -0300

    Criando a rede blockchain

--- app/controllers/blockchains_controller.rb
+++ app/controllers/blockchains_controller.rb
@@ -1,3 +1,34 @@
 class BlockchainsController < ApplicationController
+    def index
+      
+    end
+  
+    def edit
+      @blockchain = Blockchain.find(params[:id])
+    end
     
+    # GET /blockchain/new
+    def new
+      @blockchain = Blockchain.new
+    end
+  
+    def create
+      @blockchain = Blockchain.new(blockchain_params)
+    end
+    
+    def show
+      @blockchain = Blockchain.all 
+    end
+    
+    private
+  
+      def set_blockchain
+        @blockchain = Blockchain.find(params[:id])
+      end
+  
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def blockchain_params
+        params.require(:blockchain).permit(:from, :to, :what, :qty, :block_id)
+      end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   resources :transactions, only: [:show, :create, :new]
+  resources :blockchains, only: [:show, :create, :new]
+
   #get 'blockchain', to: 'transaction#index'
 
 end

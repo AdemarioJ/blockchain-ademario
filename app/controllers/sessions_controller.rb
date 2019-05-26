dc8e8028d9a08419caf58f3ce0bde525e49c2bad
ademario <ademariocc@gmail.com>
2019-05-26 01:40:50 -0300

    Layout de login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -36,7 +36,7 @@ class SessionsController < Devise::SessionsController
   # DELETE /resource/sign_out
   def destroy
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message! :notice, :signed_out if signed_out && is_flashing_format?
+    #set_flash_message! :notice, :signed_out if signed_out && is_flashing_format?
     yield if block_given?
     respond_to_on_destroy
   end
